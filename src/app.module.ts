531b43261f4d9937c3a0ab522eb333ce3575712b
Bennett <bennybenester@gmail.com>
2020-02-25 23:44:16 +0300

    :sparkles: import typeorm module to the list of imports in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './tasks/config/typeorm-config';
 
 @Module({
-imports: [ItemsModule, TasksModule],
+imports: [ItemsModule, TasksModule, TypeOrmModule.forRoot(typeormConfig)],
   controllers: [],
   providers: [],
 })
