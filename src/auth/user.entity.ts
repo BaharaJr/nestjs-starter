878a0ea51d0dca7c0761ad969e7c9862358127f5
Bennett <bennybenester@gmail.com>
2020-03-17 06:40:13 +0300

    :sparkles: add password validation method

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,26 +1,38 @@
-import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, Unique } from "typeorm";
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  Unique,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 @Unique(['username'])
 export class User extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: string;
+  @PrimaryGeneratedColumn()
+  id: string;
 
-    @Column()
-    uid: string
+  @Column()
+  uid: string;
 
-    @Column()
-    username: string
+  @Column()
+  username: string;
 
-    @Column()
-    firstname: string
-    
-    @Column()
-    lastname: string
+  @Column()
+  firstname: string;
 
-    @Column()
-    password: string
+  @Column()
+  lastname: string;
 
-    @Column()
-    salt: string
+  @Column()
+  password: string;
+
+  @Column()
+  salt: string;
+
+  async validatePassword(password: string): Promise<boolean> {
+    let hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
 }
