f3abd52acd36b8dd7e00ed14abeaa9041142e1b1
Bennett <bennybenester@gmail.com>
2020-04-01 06:18:58 +0300

    :sparkles: Implement swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,35 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { getConfiguration } from './database/config/baseconfiguration';
 import * as compression from 'compression';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as session from 'express-session';
+import * as helmet from 'helmet';
 
 const config = getConfiguration();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const options = new DocumentBuilder()
+    .setTitle('NestJs API with Postgres and Typeorm')
+    .setDescription('NestJs Starter')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(helmet());
+  app.use(
+    session({
+      secret: 'secret-key',
+      name: 'sess-tutorial',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
   app.use(compression());
   await app.listen(config.port);
+  console.log('App Listenig on Port', config.port)
 }
 bootstrap();
