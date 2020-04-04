9273dc945ab99697d2db40989447e9eb6f9c229b
Bennett <bennybenester@gmail.com>
2020-04-04 22:59:46 +0300

    :sparkles: Implement creating a task belonging to a user

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -3,20 +3,26 @@ import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './tasks.status-enum';
 import * as uid from 'uid';
+import { BadRequestException } from '@nestjs/common';
+import { User } from 'src/auth/user.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-  async createTask(createTask: CreateTaskDto) {
-    // const { title, description } = createTask;
+  async createTask(createTask: CreateTaskDto, user: User): Promise<Task> {
+    const { title, description } = createTask;
     const task = new Task();
-    Object.keys(createTask).forEach(key => {
-      task[key] = createTask[key];
-    });
-    // task.title = title;
-    // task.description = description;
-    // task.status = TaskStatus.OPEN;
-    // task.uid = uid(11);
-    await task.save();
+    // Object.keys(createTask).forEach(key => {
+    //   task[key] = createTask[key];
+    // });
+    task.title = title;
+    task.description = description;
+    task.status = TaskStatus.OPEN;
+    task.user = user;
+    if (user) {
+      await task.save();
+    } else {
+      throw new BadRequestException('User can never be null')
+    }
 
     return task;
   }
