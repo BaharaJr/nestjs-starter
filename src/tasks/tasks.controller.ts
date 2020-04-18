aac9f80d04ae036a630928253efd6eac26bb7f9c
Bennett <bennybenester@gmail.com>
2020-04-18 23:51:46 +0300

    :sparkles: Get tasks belonging to users

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,26 +1,26 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Param,
+  Controller,
   Delete,
-  Patch,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
   Query,
+  UseGuards,
   UsePipes,
   ValidationPipe,
-  ParseIntPipe,
-  UseGuards,
+  Patch,
 } from '@nestjs/common';
-import { TasksService } from './tasks.service';
-import { TaskStatus } from './tasks.status-enum';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { TasksFilterDto } from './dto/tasksfilter.dto';
-import { TaskStatusValidation } from './pipes/task-status.validation.pipe';
-import { Task } from './task.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/user.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TasksFilterDto } from './dto/tasksfilter.dto';
+import { Task } from './task.entity';
+import { TasksService } from './tasks.service';
+import { TaskStatusValidation } from './pipes/task-status.validation.pipe';
+import { TaskStatus } from './tasks.status-enum';
 
 @Controller('tasks')
 export class TasksController {
@@ -29,13 +29,15 @@ export class TasksController {
   @UseGuards(AuthGuard())
   async getTasks(
     @Query(ValidationPipe) tasksFilterDto: TasksFilterDto,
+    @GetUser() user: User,
   ): Promise<any> {
-    return this.taskService.getAllTasks(tasksFilterDto);
+    return this.taskService.getAllTasks(tasksFilterDto, user);
   }
 
   @Get('/:id')
-  getTaskById(@Param('id') id: number): Promise<Task> {
-    return this.taskService.getTaskById(id);
+  @UseGuards(AuthGuard())
+  getTaskById(@Param('id') id: number, @GetUser() user: any): Promise<Task> {
+    return this.taskService.getTaskById(id, user);
   }
 
   @Post()
@@ -49,15 +51,18 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  deleteOneTask(@Param('id', ParseIntPipe) id: number) {
-    return this.taskService.deleteOneTask(id);
+  @UseGuards(AuthGuard())
+  deleteOneTask(@Param('id', ParseIntPipe) id: number, @GetUser() user: User) {
+    return this.taskService.deleteOneTask(id, user);
   }
 
   @Patch('/:id/status')
+  @UseGuards(AuthGuard())
   async updateTaskStatus(
     @Param('id') id: number,
     @Body('status', TaskStatusValidation) status: TaskStatus,
+    @GetUser() user: User,
   ): Promise<Task> {
-    return await this.taskService.updateTaskStatus(id, status);
+    return await this.taskService.updateTaskStatus(id, status, user);
   }
 }
